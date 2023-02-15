7478f579e714c8f232ccd363203239321dc179d2
BisalBosu <bishalbosu@gmail.com>
2023-02-15 19:48:52 +0530

    logging done

--- sctipt.js
+++ sctipt.js
@@ -1,1 +1,6 @@
-console.log("HI");
+var items = document.getElementsByClassName("list-group-item");
+
+
+console.log(items);
+items[2].style.backgroundColor = "green";
+items[0].style.fontWeight = 'bold';
